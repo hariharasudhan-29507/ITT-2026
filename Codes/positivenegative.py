89487e0ac06d2c76ce4b9e355a2a7cd542c716b7
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-03-17 16:03:59 +0530

    Create positivenegative.py

---
+++ Codes/positivenegative.py
@@ -0,0 +1,8 @@
+class posneg:
+  def isPNZ(def,num):
+    res="number is positive"if num>0 else "number is negative" if num<0 else "number is zero"
+    return res
+p=posneg()
+num=int(input("enter number:"))
+res=p.isPNZ(num)
+print(res)
