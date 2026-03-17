851448b78d09354c6784b75c8a0f996c8dc00470
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-03-17 16:06:24 +0530

    Create salary.py

---
+++ Codes/salary.py
@@ -0,0 +1,6 @@
+class checkSalary:
+  def checkSal(self,salary):
+    print("no tax") if salary<=250000 else print("tax amount:",salary*0.005) if 250000<salary<500000 else print("tax amount:",salary*0.1) if 500000<salary<1000000 else print("tax amount:",salary*0.15)
+c=checkSalary()
+salary=int(input("enter salary:"))
+c.checkSal(salary)
