66f283029125a3eb7f3654c924f36b1b300ef752
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-03-17 16:26:47 +0530

    Implement coin game logic with player input

---
+++ Codes/coinGame.py
@@ -0,0 +1,41 @@
+class coinGame:
+   def coin(self,A,B):
+      if A>=B:
+         return 1
+      else:
+         return 0
+   def change(self,A,B):
+      B=A+B
+      return B
+
+c=coinGame()
+suresh=int(input("enter number of coins for suresh:"))
+sundar=int(input("enter number of coins for sundar:"))
+#if coin returns 1 that means A is the winner ,0 B is the winner
+winner=c.coin(suresh,sundar)
+raja=int(input("enter number of coins for raja:"))
+if winner==1:
+   print("N")
+   sundar=c.change(raja,sundar)
+   winner=c.coin(suresh,sundar)
+else:
+   print("S")
+   suresh=c.change(raja,suresh)
+   winner=c.coin(suresh,sundar)
+
+if winner==1:
+   print("N")
+   sundar=c.change(suresh,sundar)
+   winner=c.coin(suresh,sundar)
+   if winner==1:
+      print("N")
+   else:
+      print("S")
+else:
+   print("S")
+   suresh=c.change(sundar,suresh)
+   winner=c.coin(suresh,sundar)
+   if winner==1:
+      print("N")
+   else:
+      print("S")
