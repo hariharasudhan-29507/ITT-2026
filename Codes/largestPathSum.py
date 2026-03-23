75828f13405c13a43e761545aa00c8ded7dcb42b
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-03-23 11:08:04 +0530

    Update largestPathSum.py

--- Codes/largestPathSum.py
+++ Codes/largestPathSum.py
@@ -1,5 +1,7 @@
 #not finished
 row_count=int(input("enter row count:"))
+sum_line=[]
+line_val=0
 mat_tree=[[0 for i in range(row_count)] for j in range(row_count)]
 for i in range(row_count):
    for j in range(row_count):
@@ -10,3 +12,13 @@ for i in range(row_count):
       if i>=j:
          print(mat_tree[i][j],end=" ")
    print()
+for i in range(row_count):
+    for j in range(row_count):
+        if i>=j:
+            k=0
+            while k<row_count:
+                print(mat_tree[j][k])
+                line_val=line_val+mat_tree[i][k]
+                k=k+1
+            sum_line.append(line_val)
+print(max(sum_line))   
