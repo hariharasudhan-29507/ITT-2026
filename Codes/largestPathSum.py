847bf88d8ef114ff8a9cdc1540b6c15ccc0759a7
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-03-31 19:03:41 +0530

    optimized and error (not valid output) cleared

--- Codes/largestPathSum.py
+++ Codes/largestPathSum.py
@@ -1,24 +1,21 @@
-#not finished
-row_count=int(input("enter row count:"))
-sum_line=[]
-line_val=0
-mat_tree=[[0 for i in range(row_count)] for j in range(row_count)]
-for i in range(row_count):
-   for j in range(row_count):
-      if i>=j:
-         mat_tree[i][j]=int(input("enter element"))
-for i in range(row_count):
-   for j in range(row_count):
-      if i>=j:
-         print(mat_tree[i][j],end=" ")
-   print()
+row_count = int(input("enter row count: "))
+mat_tree = [[0 for i in range(row_count)] for j in range(row_count)]
+
 for i in range(row_count):
     for j in range(row_count):
-        if i>=j:
-            k=0
-            while k<row_count:
-                print(mat_tree[j][k])
-                line_val=line_val+mat_tree[i][k]
-                k=k+1
-            sum_line.append(line_val)
-print(max(sum_line))   
+        if i >= j:
+            mat_tree[i][j] = int(input("enter element: "))
+for i in range(row_count):
+    for j in range(row_count):
+        if i >= j:
+            print(mat_tree[i][j], end=" ")
+    print()
+dp = [[0 for i in range(row_count)] for j in range(row_count)]
+dp[0][0] = mat_tree[0][0]
+for i in range(1, row_count):
+    for j in range(i + 1):      
+        left  = dp[i-1][j-1] if j > 0 else -1
+        above = dp[i-1][j]   if j < i else -1
+        dp[i][j] = mat_tree[i][j] + max(left, above)
+       
+print("max sum =", max(dp[row_count-1][:row_count]))
