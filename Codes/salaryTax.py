56b4c9bdd207796cd4962917223d6d4e8bdb697d
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-04-20 21:45:16 +0530

    Update salaryTax.py

--- Codes/salaryTax.py
+++ Codes/salaryTax.py
@@ -1,5 +1,10 @@
+# Author - Hariharasudhan A 
 class checkSalary:
+  '''
+  simple salary computation program using condition statement
+  '''
   def checkTax(self,salary):
+    # ternary condition
     print("no tax") if salary<=250000 else print("tax amount:",salary*0.005) if 250000<salary<500000 else print("tax amount:",salary*0.1) if 500000<salary<1000000 else print("tax amount:",salary*0.15)
 c=checkSalary()
 salary=int(input("enter salary:"))
