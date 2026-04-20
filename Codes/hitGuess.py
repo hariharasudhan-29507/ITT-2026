4e2e4a45cabc6e16905991b11b0a8901f508ae34
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-04-20 21:17:29 +0530

    Update hitGuess.py

--- Codes/hitGuess.py
+++ Codes/hitGuess.py
@@ -1,12 +1,17 @@
+# Author - Hariharasudhan A
 class codehit:
     def checkhit(self, str1, str2):
         res = ""
-        l = min(len(str1), len(str2))
+        l = min(len(str1), len(str2)) 
+        '''
+        because the we are determining the hit guess for two strings
+        if we choose smaller length string it will be convinient to compare
+        '''
         for i in range(l):
             if str1[i] == str2[i]:
-                res += str(i) + "H"
+                res += str(i) + "H" # element in same index of both strings should match
             elif str1[i] in str2:
-                res += str(i) + "N"
+                res += str(i) + "N" # element should present in another string
         return res
 
 c = codehit()
