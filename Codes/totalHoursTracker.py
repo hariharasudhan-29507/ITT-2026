14f49162c57a0b8d2dc6a8d0cdedb51d14a94fd4
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-04-20 21:51:16 +0530

    Update totalHoursTracker.py

--- Codes/totalHoursTracker.py
+++ Codes/totalHoursTracker.py
@@ -1,10 +1,17 @@
+# Author - Hariharasudhan A
 class TotalHoursTracker:
         def track(self,time_unit,hours):
-                perday = 24
-                full_session = hours//perday
+                '''
+                from the given total hours and unit time 
+                we need to find full session work in hours 
+                and extra session work in hours
+                '''
+                perday = 24 # perday - 24 hrs
+                full_session = hours//perday 
 
                 extra_session = time_unit - full_session
 
+                # finding full hours and extra hours from given time unit and hours
                 full_hours = full_session*perday
                 extra_hours = (hours - full_hours) / extra_session
 
@@ -16,12 +23,13 @@ time_unit = int(input("Enter time units : "))
 hours = int(input("Enter Hours : "))
 
 flag = 0
+# checking constraints
 flag = 1 if (time_unit < hours and hours >= 24 and hours % 2 == 0) else 0
 
 full_session , extra_session = 0,0
 if flag == 1:
         full_session , extra_session = t.track(time_unit,hours)
-
+        # printing it in console
         print("full session :",full_session , "\nextra session :",extra_session)
 
 else :
