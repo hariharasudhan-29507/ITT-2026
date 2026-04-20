95da4b976d65304ca9dfee19e2eaa03a81c45a38
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-04-20 17:56:16 +0530

    Update coinGame.py

--- Codes/coinGame.py
+++ Codes/coinGame.py
@@ -1,3 +1,4 @@
+# Author - Hariharasudhan A
 class coinGame:
    def coin(self,A,B):
       if A>=B:
@@ -9,11 +10,16 @@ class coinGame:
       return B
 
 c=coinGame()
+'''
+coin swap occurs with every loser of the game // done using change() method
+'''
 suresh=int(input("enter number of coins for suresh:"))
 sundar=int(input("enter number of coins for sundar:"))
 #if coin returns 1 that means A is the winner ,0 B is the winner
 winner=c.coin(suresh,sundar)
 raja=int(input("enter number of coins for raja:"))
+
+# match result 
 if winner==1:
    print("N")
    sundar=c.change(raja,sundar)
