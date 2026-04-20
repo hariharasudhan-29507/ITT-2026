41a620da78358837156e2646b7bd1f65493b4c5a
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-04-20 21:35:34 +0530

    Update safeDivision.py

--- Codes/safeDivision.py
+++ Codes/safeDivision.py
@@ -1,16 +1,21 @@
+# Author - Hariharasudhan A
 class safeDivision:
+   '''
+   simple exception throw cases on basic arithmetic division
+   '''
    def divide(self,num1,num2):
-      try:
+      try:# try block
         div=num1/num2
         return div
-      except ValueError:
+      except ValueError: # catch 1
          print("Error: Not a Number")
-      except ZeroDivisionError:
+      except ZeroDivisionError: # catch 2
          print("Error: Division by zero")
 
 s=safeDivision()
 numbers=input("enter numbers:")
 
+# get input as string and parse them and feed them in input as int
 nums=numbers.split(" ")
 
 value=s.divide(int(nums[0]),int(nums[1]))
