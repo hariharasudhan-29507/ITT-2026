16797c6d91e02d5c3c7285a914feebec523d0939
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-04-20 21:58:50 +0530

    Update vowelConsonant.py

--- Codes/vowelConsonant.py
+++ Codes/vowelConsonant.py
@@ -1,5 +1,9 @@
+# Author - Hariharasudhan A
 class VowelConsonant:
-  def isVorC(self,char):
+  '''
+  simple vowel or consonant checking
+  '''
+  def isVorC(self,char): # ternary condition
     print("given char is vowel") if char in "aeiou" else print("given char is consonant")
 v=VowelConsonant()
 char=input("enter a character:")
