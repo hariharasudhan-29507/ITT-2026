cbcb75d7ddb1f972a0054a0976bbd6349f6a512f
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-04-20 17:46:09 +0530

    Update Large3.py

--- Codes/Large3.py
+++ Codes/Large3.py
@@ -1,8 +1,14 @@
+# Author - Hariharasudhan A
 class LargeNum:
-  def isLarge(self,a,b,c):
+  '''
+  simple largest among three numbers
+  '''
+  def isLarge(self,a,b,c): # ternary condition 
     print("number 1 is largest") if a>b and a>c else print("number 2 is largest") if b>c else print("number 3 is largest")
+# object creation
 l=LargeNum()
 a=int(input("enter number 1:"))
 b=int(input("enter number 2:"))
 c=int(input("enter number 3:"))
+# function calling
 l.isLarge(a,b,c)
