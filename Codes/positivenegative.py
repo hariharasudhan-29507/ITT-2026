e1d1dc610242bd06425a36f8b1562f89ec460a93
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-04-20 21:31:14 +0530

    Update positivenegative.py

--- Codes/positivenegative.py
+++ Codes/positivenegative.py
@@ -1,5 +1,10 @@
+# Author - Hariharasudhan
 class posneg:
+  '''
+  simple positive or negative or zero finder 
+  '''
   def isPNZ(def,num):
+    # ternary condition
     res="number is positive"if num>0 else "number is negative" if num<0 else "number is zero"
     return res
 p=posneg()
