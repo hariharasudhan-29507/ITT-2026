32d20856c933d0b715ec7bc711f8a30e3a07b443
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-04-20 21:33:08 +0530

    Update removeReplace.py

--- Codes/removeReplace.py
+++ Codes/removeReplace.py
@@ -1,14 +1,18 @@
+# Author - Hariharasudhan A
 class remRep:
+  '''
+  remove target and replace it with place holder
+  '''
   def Function(self,nums,val):
     l=len(nums)
     k=0
     print("[",end="")
     for i in range(0,l):
-      if nums[i]!=val:
+      if nums[i]!=val: # only elements that are not target
         k=k+1
         print(nums[i],",",end="")
     for i in range (0,l-k):
-        print("_",",",end="")
+        print("_",",",end="") # place holders
     print("]",end="")
 r=remRep()
 nums=eval(input("enter list:"))
