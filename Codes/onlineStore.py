a8d6c402294a3d91fdd5eb4c1475ce3c06726094
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-04-20 21:30:05 +0530

    Update onlineStore.py

--- Codes/onlineStore.py
+++ Codes/onlineStore.py
@@ -1,4 +1,8 @@
+# Author - Hariharasudhan A
 class store:
+  '''
+  simple bill calculation using condition statement
+  '''
   def Bill(self,amount):
     if amount>=5000:
       d_amount=amount*0.02
@@ -8,7 +12,9 @@ class store:
       amount-=d_amount
     else:
       print("enter valid amount")
+# object declaration
 s=store()
 amount=int(input("enter the purchase amount:"))
+# function calling
 Total_amount=s.Bill(amount)
 print("bill amount",Total_amount)
