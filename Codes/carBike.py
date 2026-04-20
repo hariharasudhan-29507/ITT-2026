7ba795fb63345ebc0b5683578801a9bca072429d
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-04-20 17:51:26 +0530

    Update carBike.py

--- Codes/carBike.py
+++ Codes/carBike.py
@@ -1,5 +1,10 @@
+# Author - Hariharasudhan A
 print("Ravi's shop")
 prb=0
+'''
+car need 4 tyres , bike need 2 tyres 
+cars are the 1st priority to make with those tyres 
+'''
 tyre_count=int(input("Enter number of tyres:"))
 if  2<= tyre_count <= 100:
    if tyre_count%2==0:
@@ -7,7 +12,7 @@ if  2<= tyre_count <= 100:
          prb=0
       else:
          prb=1
-print("Will Ravi's friend be able to buy a bike ?")
+print("Will Ravi's friend be able to buy a bike ?") # need a tyre value where bike can be made
 if prb==1:
    print("YES")
 else:
